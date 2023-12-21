fa7945ba39613ec8fa140b8ceb05381a37ec9681
huakyouin <jxh0706@126.com>
2023-12-22 02:34:04 +0800

    build cnn

--- models/network.py
+++ models/network.py
@@ -6,19 +6,26 @@ import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 import math
 
-class MelRNN(nn.Module):
-    def __init__(self, input_size=80, hidden_size=128, num_layers=2, num_classes=2):
-        super(MelRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+class MelCNN(nn.Module):
+    def __init__(self, input_size, num_classes=2):
+        super(MelCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # 根据传入的input_size动态调整全连接层的输入尺寸
+        self.fc_input_features = 64 * (input_size[0] // 4) * (input_size[1] // 4)
+        self.fc1 = nn.Linear(self.fc_input_features, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size, device=x.device)  # 初始化隐藏状态
-        out, _ = self.rnn(x, h0)
-        out = self.fc(out[:, -1, :])  # 取RNN最后一个时间步的输出作为模型的输出
-        return out
+        x = x.unsqueeze(1)  # [batch,w,h] -> [batch,channel=1,w,h]
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # 使用nn.Flatten()层展平
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
     
 class MelTransformer(nn.Module):
     def __init__(self, input_size=80, hidden_size=128, num_layers=10, num_classes=2):
