600e5695129aa6f10d21e9dcd59929d01fd37715
huakyouin <jxh0706@126.com>
2023-12-27 04:46:37 +0800

    report

--- data/datasets.py
+++ data/datasets.py
@@ -22,11 +22,38 @@ class MelFrequencyDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+    def get_length_statistics(self):
+        lengths = [np.load(file).shape[0] for file in self.data]
+        max_length = max(lengths)
+        min_length = min(lengths)
+        avg_length = sum(lengths) / len(lengths)
+        percentile50_length = np.percentile(lengths, 50)
+        percentile25_length = np.percentile(lengths, 25)
+        percentile75_length = np.percentile(lengths, 75)
+        return {
+            "max_length": max_length,
+            "min_length": min_length,
+            "avg_length": avg_length,
+            "percentile25_length": percentile25_length,
+            "percentile50_length": percentile50_length,
+            "percentile75_length": percentile75_length
+        }
+    
+    def cut_left_useless(self,arr):
+        # 寻找第一列非空白的位置
+        non_negative_index = np.argmax(arr != -11.512925, axis=0)
+        # 去除左侧空白列的部分
+        trimmed_matrix = arr[ non_negative_index.min():]
+        return trimmed_matrix
+
     def __getitem__(self, idx):
         file_path = self.data[idx]
         label = self.labels[idx]
         # 加载梅尔频率数据
         mel_data = np.load(file_path)  # 假设是.npy格式的文件
+        # 去除左侧空白
+        mel_data = self.cut_left_useless(mel_data)
+        t_len = mel_data.shape[0]
         # 对数据进行填充或截断
         if mel_data.shape[0] < self.max_seq_length:
             # 填充到最大长度
@@ -36,4 +63,4 @@ class MelFrequencyDataset(Dataset):
             # 截断到最大长度
             mel_data = mel_data[:self.max_seq_length, :]
         # 将梅尔频率特征转换为PyTorch张量并返回
-        return torch.tensor(mel_data), torch.tensor(label)
+        return torch.tensor(mel_data), torch.tensor(label) , t_len
